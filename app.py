d0e0a6170f42d45c1cbf5fbcad748cd7e8c4098c
MatheusVSMPimentel <matheusvsm.pimentel@hotmail.com>
2023-09-01 19:04:40 -0300

    Create Database, create migration, work controllers and templates

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from models.Veiculo import Veiculo
+from flask_migrate import Migrate
+
+from models.Veiculo import db
+from routes.veiculos_bp import veiculos_bp
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(veiculos_bp, url_prefix='/veiculos')
 
 
-@app.route('/hello/')
-def hello():
-    return render_template('index.html', name= Veiculo())
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
--- models/Veiculo.py
+++ models/Veiculo.py
@@ -1,10 +1,42 @@
-class Veiculo:
-    Tipo = 0
-    Cor = "Branco"
-    Marca = "Citroen"
-    Modelo = "C4"
-    AnoFabricacao = "2015"
-    Novo = True
-    KmRodados = 0
-    Leilao = False
-    Parcela = True
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Veiculo(db.Model):
+    __tablename__ = 'Veiculo'
+    Id = db.Column(db.Integer, primary_key=True)
+    Tipo = db.Column(db.Integer)
+    Cor = db.Column(db.String(70))
+    Marca = db.Column(db.String(70))
+    Modelo = db.Column(db.String(70))
+    AnoFabricacao = db.Column(db.String(20))
+    Novo = db.Column(db.Boolean)
+    KmRodados = db.Column(db.Integer)
+    Leilao = db.Column(db.Boolean)
+    Parcela = db.Column(db.Boolean)
+    
+    def __init__(self, tipo:int, cor:str, marca:str, modelo:str, anoFabricacao:str, novo:bool, kmRodados:int, leilao:bool,parcela:bool ):
+        self.Tipo = tipo
+        self.Cor = cor
+        self.Marca = marca
+        self.Modelo = modelo
+        self.AnoFabricacao = anoFabricacao
+        self.Novo = novo
+        self.KmRodados = kmRodados
+        self.Leilao = leilao
+        self.Parcela = parcela
+
+    @property
+    def serialize(self):
+        return {
+            'Id': self.Id,
+            'Tipo': self.Tipo,
+            'Cor': self.Cor,
+            'Marca': self.Marca,
+            'Modelo': self.Modelo,
+            'AnoFabricacao': self.AnoFabricacao,
+            'Novo': self.Novo,
+            'KmRodados': self.KmRodados,
+            'Leilao': self.Leilao,
+            'Parcela': self.Parcela
+        }
